c622753c324c57e2053512f133b018a692922a0c
Aashish <ajmeraaashu55@gmail.com>
2023-03-11 18:00:16 +0530

    lecture 105 is complete

---
+++ Java script/objects and array/105array01.js
@@ -0,0 +1,15 @@
+// first type
+
+let arry1 = [1,2,4,5,"Lover boy", true]
+console.log(arry1);
+
+// second type 
+
+let arry2 = new Array(1,2,4,5,"Lover boy", true);
+console.log(arry2);
+
+// array ki value me kuchh change krna ho to 
+
+arry1[0]=33;  // frist array me ke 0 index ki value me 33 se replace krne me 
+console.log(arry1);
+
