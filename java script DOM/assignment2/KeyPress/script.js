5a85084ae1810ed5366ef5450b70e06e902221f6
Aashish <ajmeraaashu55@gmail.com>
2023-04-02 09:43:57 +0530

    project file update

--- java script DOM/assignment2/KeyPress/script.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("input");
-const log = document.getElementById("log");
-
-input.addEventListener("keypress", logKey);
-
-function logKey(e) {
-  log.textContent += ` ${e.code}`;
-}
--- java script DOM/assignment2/mouseOver/script.js
+++
@@ -1,6 +0,0 @@
-let mouse_over = document.querySelector(".mouseOver_");
-
-document.body.addEventListener("mouseover", () => {
-    mouse_over.style.color = "red"
-    document.body.style.backgroundColor = "grey"
-});
