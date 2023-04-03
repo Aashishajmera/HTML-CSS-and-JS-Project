a3e22ad1e44fb9d832ce221396c510bab626f3f5
Aashish <ajmeraaashu55@gmail.com>
2023-04-03 08:21:31 +0530

    remove my compute file

--- myComputer/Java script/project's/java script DOM project/assignment2/mouseOut/script.js
+++
@@ -1,7 +0,0 @@
-let mouse_out = document.querySelector(".mouseOut");
-
-mouse_out.addEventListener("mouseout", () => {
-    mouse_out.style.color = "red"
-    document.body.style.backgroundColor = "grey"
-  
-});
