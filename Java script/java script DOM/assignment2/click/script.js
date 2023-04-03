ca008800698b551babd9c8d4e431dc2ef2134e34
Aashish <ajmeraaashu55@gmail.com>
2023-04-03 09:38:08 +0530

    add file in one folder

--- Java script/java script DOM/assignment2/click/script.js
+++
@@ -1,7 +0,0 @@
-let prgph = document.querySelector("p")
-
-prgph.addEventListener("click", () => {
-    // alert("hiii guy's")
-    prgph.innerText = "Ok done one time click ";
-    console.log(prgph.innerText); 
-})
--- Java script/java script DOM/assignment2/dblclick/script.js
+++
@@ -1,7 +0,0 @@
-let span_tag = document.querySelector("span")
-
-document.body.addEventListener("dblclick", dblclick_function);
-
-function dblclick_function() {
-    span_tag.style.backgroundColor = "blue";
-}
