840c38b039b0f48e4271166d8a2ddf82d6f5bc5a
Aashish ajmera <122967132+Aashishajmera@users.noreply.github.com>
2023-06-05 14:47:55 +0530

    Update script.js
    delete code

--- project's/Java script/project's/calculator/script.js
+++ project's/Java script/project's/calculator/script.js
@@ -1,16 +1,1 @@
-// let froot = "banana";
-// switch (froot) {
-//   case "mango":
-//     console.log("mango 150 rupes");
-//     break;
-//   case "banana":
-//     console.log("banana 50 rupes");
-//     break;
-//     default:
-//         console.log("sorry");
-// }
-
-
-// if else 
-
 
